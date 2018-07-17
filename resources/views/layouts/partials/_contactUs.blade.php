130d566eaeafe84bbed69722d253ed57d9f18173
Jesus Moreno Becerril <jesus@gendra.mx>
2018-07-17 18:09:27 -0500

    Partials for all sections created

---
+++ resources/views/layouts/partials/_contactUs.blade.php
@@ -0,0 +1,22 @@
+<!-- Footer top section -->
+    <!--
+    <section class="footer-top-section spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 footer-top-content">
+                    <div class="section-title title-left">
+                        <h2>Contact Us</h2>
+                    </div>
+                    <h3>New York, USA</h3>
+                    <p>207 Park Avenue New York, NY 90210</p>
+                    <p><span>Email:</span> info@colorlib.com</p>
+                    <h4>Phone:</h4>
+                    <h5>+1 (409) 987–4567</h5>
+                </div>
+            </div>
+        </div>
+        -->
+        <!--
+        <div class="map-area" id="map-canvas"></div>
+    </section>-->
+    <!-- Footer top section end-->
--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -1,10 +1,26 @@
-<!-- Header -->
-    <header>
-        <div class="container">
-            <div class="intro-text">
-                <div class="intro-lead-in">Welcome To Our Studio!</div>
-                <div class="intro-heading">It's Nice To Meet You</div>
-                <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+<!-- Header section  -->
+    <header class="header-section">
+        <div class="container">     
+            <!-- logo -->
+            <h3 ><img src="img/logo.png" style="max-height: 35px;">
+            <!--<a href="" class="site-btn hidden-xs">send donation</a>-->
+            <!-- nav menu -->
+            <div class="nav-switch">
+                <i class="fa fa-bars"></i>
             </div>
+<!-----------------------------------------------MAIN MENU------------------------------------------------------------>         
+            <nav class="main-menu" >
+                <ul >
+                    <li class="active" ><a href="index.html">Inicio</a></li>
+                    <li><a href="about.html">Nosotros</a></li>
+                    <li><a href="sermons.html">Proyectos</a></li>
+                <!--    <li><a href="event.html">Event</a></li> -->
+                    <li><a href="blog.html">Cursos</a></li>
+                    <li><a href="contact.html">Contacto</a></li>
+                </ul>
+            </nav>
+            </h3>
+<!-----------------------------------------------END OF MAIN MENU------------------------------------------------------------>
         </div>
     </header>
+    <!-- Header section end -->
