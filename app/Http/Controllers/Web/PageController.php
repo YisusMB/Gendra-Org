4d3e2c41f51df773d402300cdd5e97b42d47a837
Jesus Moreno Becerril <jesus@gendra.mx>
2018-07-25 17:36:53 -0500

    Removed register page | added button functions *ver* for course | added Admin/profiles for CRUD

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -33,9 +33,4 @@ class PageController extends Controller
     	return view('layouts.pages.curso', compact('course'));
     }
 
-    public function admin(){
-        $courses = Course::orderBy('id', 'DESC')->paginate(30);
-        $profiles = Profile::orderBy('id', 'DESC')->paginate(30);
-        return view('layouts.pages.admin', compact('courses','profiles'));
-    }
 }
